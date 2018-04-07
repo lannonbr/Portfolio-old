dface4cdf3d7428c1367d76fea01675c6e46f6a1
Benjamin Lannon <lannonbr@clarkson.edu>
2018-04-07 16:01:19 -0400

    Switching from 2 <nav> elements to a single <nav>

--- js/main.js
+++ js/main.js
@@ -1,21 +1,24 @@
-function toggleNav() {
-  let nav = document.getElementsByTagName("nav")[1];
-
-  if (nav.className == "nav-mobile") {
-    nav.className = "nav-mobile--open";
-  } else {
-    nav.className = "nav-mobile--closed";
-    setTimeout(function() {
-      nav.className = "nav-mobile";
-    }, 300);
-  }
-}
-
 window.onload = function() {
   // Feature test for WebP images
   let hero = document.getElementById("hero");
   if (canUseWebP()) hero.classList = "webp";
   else hero.classList = "no-webp";
+
+  const menuButton = document.getElementById("menu-button");
+  const nav = document.getElementsByTagName("nav")[0];
+
+  menuButton.onclick = function() {
+    if (nav.className == "") {
+      nav.className = "nav-open";
+      menuButton.className = "fa fa-times";
+    } else {
+      nav.className = "nav-close";
+      menuButton.className = "fa fa-bars";
+      setTimeout(function() {
+        nav.className = "";
+      }, 300);
+    }
+  };
 
   navigator.serviceWorker.getRegistrations().then(function(registrations) {
     for (let registration of registrations) {
